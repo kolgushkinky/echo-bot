5a5e8245fe9e48243a3f62274afaa984c7e41c75
Gianluca Recchia <g.recchia@desotech.it>
2021-05-07 16:57:04 +0200

    Implement getUpdates

---
+++ bot/getupdates.go
@@ -0,0 +1,22 @@
+package bot
+
+import (
+	"net/url"
+	"strconv"
+)
+
+type getUpdatesResponse struct {
+	Ok     bool     `json:"bool"`
+	Result []Update `json:"result"`
+}
+
+func (b *Bot) GetUpdates(offset int64) ([]Update, error) {
+	data := url.Values{}
+	data.Add("offset", strconv.FormatInt(offset, 10))
+	var apiResponse getUpdatesResponse
+	err := b.httpClient.Post("getUpdates", data, &apiResponse)
+	if err != nil {
+		return nil, err
+	}
+	return apiResponse.Result, nil
+}
